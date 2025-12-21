3d939bf59bcd4984a8b9cf01068940c491c1f471
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:10:09 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoGoogleMyMaps/Data/ABSGoogleMyMaps.cs
+++ SunamoGoogleMyMaps/Data/ABSGoogleMyMaps.cs
@@ -1,6 +1,4 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-﻿namespace SunamoGoogleMyMaps.Data;
+namespace SunamoGoogleMyMaps.Data;
 
 /// <summary>
 ///     Implicitly are strings.Empty to avoid cheching data class for null
--- SunamoGoogleMyMaps/Data/ABTGoogleMyMaps.cs
+++ SunamoGoogleMyMaps/Data/ABTGoogleMyMaps.cs
@@ -1,6 +1,4 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-﻿namespace SunamoGoogleMyMaps.Data;
+namespace SunamoGoogleMyMaps.Data;
 
 public class ABTGoogleMyMaps<Key, Value>(Key a, Value b)
 {
